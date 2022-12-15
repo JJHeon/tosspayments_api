ab7eab7fa2957eadafd6e6c2f71294a4e3bd2a85
JH <wasicrob27@gmail.com>
2022-12-15 20:59:02 +0900

    add first index.html

---
+++ src/main/java/example/tosspayments/IndexController.java
@@ -0,0 +1,12 @@
+package example.tosspayments;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String gate(){
+        return "index";
+    }
+}
