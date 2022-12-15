d7c9f19d4550f5192f0324683e3333bd10f22cba
JH <wasicrob27@gmail.com>
2022-12-15 20:50:35 +0900

    init first setting

---
+++ src/main/java/example/tosspayments/TosspaymentsApplication.java
@@ -0,0 +1,13 @@
+package example.tosspayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosspaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosspaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/tosspayments/TosspaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package example.tosspayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosspaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
